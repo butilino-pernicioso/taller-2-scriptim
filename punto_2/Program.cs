e8d5f438cfc1a3668049c2f99e45f36a932f8d8e
B09S116est <B09S116est@MEDAULAS.UPB>
2024-04-04 12:59:50 -0500

    make point2

---
+++ punto_2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace punto_2
+{
+    internal class Program
+    {
+        
+        public delegate string Cocinar (string c);
+
+        static void Main(string[] args)
+        {
+            string alimento;
+
+            Cocinar delegado ;
+
+
+            delegado = Cortar;
+            delegado += Hervir;
+            delegado += Saltear;
+            
+
+            Console.WriteLine("ingrese el alimento de va cortar, hervir, saltear");
+            alimento=Console.ReadLine();
+
+            delegado.Invoke(alimento);
+            
+
+        }
+
+        public static string Hervir (string c) 
+        {
+            string ret;
+            ret = c + " hervir";
+
+            return  ret ;
+        }
+
+        public static string Saltear (string c)
+        {
+            string ret;
+            ret = c + " saltear";
+
+            return ret;
+        }
+
+        public static string Cortar (string c) 
+        {
+            string ret;
+            ret = c + " cortar";
+
+            return ret;
+        }
+    }
+}
